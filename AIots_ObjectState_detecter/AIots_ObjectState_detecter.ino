781c52e79aae8a7163496028835758097e10f3df
0hotpotman0 <fenyishi0@gmail.com>
2021-10-08 14:14:55 +0800

    new project

---
+++ AIots_ObjectState_detecter/AIots_ObjectState_detecter.ino
@@ -0,0 +1,576 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#include <WT_3Asix_inferencing.h>
+//#include <accel_gesture_spectral_inference.h>
+#include"LIS3DHTR.h"
+#include "SoftwareSerial2.h"
+#include "gps.h"
+TFT_eSPI tft = TFT_eSPI();           // Invoke custom library
+TFT_eSprite spr = TFT_eSprite(&tft); // Sprite
+LIS3DHTR<TwoWire> lis;
+
+#include "DHT.h"
+#define DHTPIN 0      // what pin we're connected to
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define CONVERT_G_TO_MS2    9.80665f
+static bool debug_nn = false;
+
+SoftwareSerial1 softSerial2(40, 41);
+
+//TFT_eSPI tft;
+#include "fonts.h"
+#define X_OFFSET 2
+#define Y_OFFSET 0
+#define X_SIZE 80
+#define Y_SIZE 20
+#define R_SIZE 4
+#define BOX_SPACING 2
+
+#define TFT_GRAY 0b1010010100010000
+#define TFT_GRAY10 0b0100001100001000
+#define TFT_GRAY20 0b0010000110000100
+
+#define HIST_X_OFFSET 2
+#define HIST_Y_OFFSET 75
+#define HIST_X_SIZE 315
+#define HIST_X_TXTSIZE X_SIZE - 3
+#define HIST_Y_SIZE 160
+#define HIST_X_BAR_OFFSET 50
+#define HIST_X_BAR_SPACE 2
+
+static int P_temp, P_humi, P_is_exist = 1, P_is_join = 1;
+static char recv_buf[512];
+static bool is_exist = false;
+static bool is_join = false;
+static int led = 0;
+static bool Lora_is_busy = false;
+static int temp = 0;
+static int humi = 0;
+static int switch_UI = 0;
+
+float Stop;
+float Turn;
+float Wave;
+
+char str[100] = {0};
+char str_devEui[50] = {0};
+char str_appEvi[50] = {0};
+//static int  Lora_Ack_timeout;
+
+enum e_module_Response_Result {
+  MODULE_IDLE,
+  MODULE_RECEIVING,
+  MODULE_TIMEOUT,
+  MODULE_ACK_SUCCESS,
+};
+
+enum e_module_AT_Cmd {
+  AT_OK,
+  AT_ID,
+  AT_MODE,
+  AT_DR,
+  AT_CH,
+  AT_KEY,
+  AT_CLASS,
+  AT_PORT,
+  AT_JOIN,
+  AT_CMSGHEX,
+  AT_TIMOUT,
+};
+
+int module_AT_Cmd = 0;
+typedef struct s_E5_Module_Cmd {
+  char p_ack[15];
+  int timeout_ms;
+  char p_cmd[70];
+} E5_Module_Cmd_t;
+E5_Module_Cmd_t E5_Module_Cmd[] = {
+  {"+AT: OK", 1000, "AT\r\n"},
+  {"+ID: AppEui", 1000, "AT+ID\r\n"},
+  {"+MODE", 1000, "AT+MODE=LWOTAA\r\n"},
+  {"+DR", 1000, "AT+DR=US915\r\n"},
+  {"CH", 1000, "AT+CH=NUM,0-2\r\n"},
+  {"+KEY: APPKEY", 1000, "AT+KEY=APPKEY,\"2B7E151628AED2A6ABF7158809CF4F3C\"\r\n"},
+  {"+CLASS", 1000, "AT+CLASS=A\r\n"},
+  {"+PORT", 1000, "AT+PORT=8\r\n"},
+  {"Done", 10000, "AT+JOIN\r\n"},
+  {"Done", 30000, ""},
+};
+
+static int at_send_check_response(char *p_ack, int timeout_ms, char *p_cmd, ...) {
+  int ch;
+  int num = 0;
+  int index = 0;
+  int startMillis = 0;
+  va_list args;
+  memset(recv_buf, 0, sizeof(recv_buf));
+  va_start(args, p_cmd);
+  softSerial2.printf(p_cmd, args);
+  Serial.printf(p_cmd, args);
+  va_end(args);
+
+  startMillis = millis();
+
+  if (p_ack == NULL) {
+    return 0;
+  }
+  do {
+    while (softSerial2.available() > 0)
+    {
+      ch = softSerial2.read();
+      recv_buf[index++] = ch;
+      Serial.print((char)ch);
+      delay(2);
+    }
+
+    if (strstr(recv_buf, p_ack) != NULL) {
+      return 1;
+    }
+
+  } while (millis() - startMillis < timeout_ms);
+  return 0;
+}
+
+static int at_send_check_response_flag(int timeout_ms, char *p_cmd, ...) {
+  if (Lora_is_busy == true) {
+    return 0;
+  }
+  Lora_is_busy = true;
+  va_list args;
+  memset(recv_buf, 0, sizeof(recv_buf));
+  va_start(args, p_cmd);
+  softSerial2.printf(p_cmd, args);
+  Serial.printf(p_cmd, args);
+  va_end(args);
+  return 1;
+}
+
+static int check_message_response() {
+  static bool init_flag = false;
+  static int startMillis = 0;
+  static int index = 0;
+  e_module_Response_Result result = MODULE_ACK_SUCCESS;
+  int ch;
+  int num = 0;
+  if (Lora_is_busy == true) {
+    if (init_flag == false) {
+      startMillis = millis();
+      init_flag = true;
+      index = 0;
+      memset(recv_buf, 0, sizeof(recv_buf));
+      Serial.println("Cmd Start......");
+    }
+    Lora_is_busy = false;
+    init_flag = false;
+    while (softSerial2.available() > 0) {
+      ch = softSerial2.read();
+      recv_buf[index++] = ch;
+      Serial.print((char)ch);
+      delay(2);
+    }
+
+    if (strstr(recv_buf, E5_Module_Cmd[module_AT_Cmd].p_ack) != NULL) {
+      //          is_join = true;
+      return result;
+    }
+
+    if (millis() - startMillis >= E5_Module_Cmd[module_AT_Cmd].timeout_ms) {
+      Serial.println("Cmd Timeout......");
+      return MODULE_TIMEOUT;
+    }
+    Lora_is_busy = true;
+    init_flag = true;
+    return MODULE_RECEIVING;
+  }
+  return MODULE_IDLE;
+}
+
+static int init_ui = 0;
+void display_EVI() {
+  if (init_ui == 0) {
+    init_ui = 1;
+    tft.setFreeFont(FMO12);
+    tft.fillRect(0, 0, 320, 30, TFT_BLUE);      //Drawing rectangle with blue fill
+    tft.drawRoundRect(0, 0, 320, 30, 6, TFT_YELLOW);
+    tft.setTextColor(TFT_WHITE);                //Setting text color
+    tft.drawString("GPS & WT State", 45, 5); //Drawing text string
+
+
+    tft.setFreeFont(FM9);
+    tft.setTextColor(TFT_WHITE);
+    tft.drawString("Device", 5, 40, GFXFF);
+    tft.drawRoundRect(HIST_X_OFFSET, HIST_Y_OFFSET - 20, HIST_X_SIZE, HIST_Y_SIZE + 20, R_SIZE, TFT_WHITE);
+
+    tft.setFreeFont(FSSO9);
+    tft.setTextColor(TFT_BLUE);
+    tft.drawString("LoRaWAN", HIST_X_OFFSET + 10, HIST_Y_OFFSET , GFXFF);
+    tft.setFreeFont(FM9);
+    tft.setTextColor(TFT_WHITE);
+    tft.fillRect(HIST_X_OFFSET + 10, HIST_Y_OFFSET + 25, 300, 15, TFT_BLACK);
+    strcpy(str, "DevEui:");
+    strcat(str, str_devEui);
+    tft.drawString(str, HIST_X_OFFSET + 10, HIST_Y_OFFSET + 25, GFXFF);
+    memset(str, 0, sizeof(str));
+    tft.fillRect(HIST_X_OFFSET + 10, HIST_Y_OFFSET + 45, 300, 15, TFT_BLACK);
+    strcpy(str, "AppEui:");
+    strcat(str, str_appEvi);
+    tft.drawString(str, HIST_X_OFFSET + 10, HIST_Y_OFFSET + 45, GFXFF);
+    tft.drawString("AppKey:", HIST_X_OFFSET + 10, HIST_Y_OFFSET + 65, GFXFF);
+    tft.drawString("\"", HIST_X_OFFSET + 10, HIST_Y_OFFSET + 65, GFXFF);
+    tft.drawString("2B7E151628AED2A6AB", HIST_X_OFFSET + 88, HIST_Y_OFFSET + 65, GFXFF);
+    tft.drawString("F7158809CF4F3C", HIST_X_OFFSET + 88, HIST_Y_OFFSET + 85, GFXFF);
+  }
+}
+
+
+
+void refreshGpsInfo() {
+  int xOffset, yOffset;
+  //  static int init_ui = 0;
+  if (init_ui == 0) {
+    tft.fillRect(HIST_X_OFFSET, HIST_Y_OFFSET - 18, HIST_X_TXTSIZE, 18, TFT_BLACK);
+    tft.fillRect(HIST_X_OFFSET, HIST_Y_OFFSET, HIST_X_SIZE, HIST_Y_SIZE, TFT_BLACK);
+
+    tft.setFreeFont(FMO12);
+    tft.fillRect(0, 0, 320, 30, TFT_BLUE);      //Drawing rectangle with blue fill
+    tft.setTextColor(TFT_WHITE);                //Setting text color
+    tft.drawRoundRect(0, 0, 320, 30, 6, TFT_YELLOW);
+    tft.drawString("GPS & WT State", 45, 5); //Drawing text string
+    tft.setFreeFont(&FreeMono9pt7b);
+    tft.drawString("Device:", 5, 45);
+    tft.drawString("STATE:", 146, 45);
+
+    tft.setFreeFont(&FreeMono9pt7b);
+    tft.setTextColor(TFT_GREEN);
+    tft.drawString("Predict Result:", 5, 88, 1); // Print the test text in the custom font
+    tft.drawRoundRect(185, 75, 100, 40, 5, TFT_WHITE);
+
+    tft.setFreeFont(&FreeSansBoldOblique9pt7b);
+    //    tft.drawString("%", 160, 85, 1);
+
+    tft.setFreeFont(FM9);
+    tft.setTextColor(TFT_WHITE);
+
+    xOffset = HIST_X_OFFSET + 10;
+    yOffset = HIST_Y_OFFSET + 50;
+    tft.drawString("Date: ", xOffset, yOffset, GFXFF);
+    tft.drawString(N_date, xOffset + 50, yOffset, GFXFF);
+    yOffset += 18;
+    tft.drawString("Time: ", xOffset, yOffset, GFXFF);
+    tft.drawString(N_time, xOffset + 50, yOffset, GFXFF);
+    yOffset += 18;
+    tft.drawString("LAT: ", xOffset, yOffset, GFXFF);
+    tft.drawString(N_lat, xOffset + 50, yOffset, GFXFF);
+    yOffset += 18;
+    tft.drawString("LONG: ", xOffset, yOffset, GFXFF);
+    tft.drawString(N_lng, xOffset + 50, yOffset, GFXFF);
+    yOffset += 18;
+    tft.drawString("ALT: ", xOffset, yOffset, GFXFF);
+    tft.drawString(N_meters, xOffset + 50, yOffset, GFXFF);
+    yOffset += 18;
+    tft.drawString("Satellites: ", xOffset, yOffset, GFXFF);
+    tft.drawString(N_satellites, xOffset + 120, yOffset, GFXFF);
+
+    tft.drawRoundRect(HIST_X_OFFSET, HIST_Y_OFFSET + 45, HIST_X_SIZE, HIST_Y_SIZE - 45, R_SIZE, TFT_WHITE);
+    //    ui.previous_display = ui.selected_display;
+  }
+
+  if (is_exist != P_is_exist || init_ui == 0) {
+    if (is_exist) {
+      tft.fillRect(80, 45, 65, 15, TFT_BLACK);
+      tft.setTextColor(TFT_GREEN);
+      tft.drawString("find", 80, 45, GFXFF);
+    }
+    else {
+      tft.fillRect(80, 45, 65, 15, TFT_BLACK);
+      tft.setTextColor(TFT_RED);
+      tft.drawString("unfind", 80, 45, GFXFF);
+    }
+    P_is_exist = is_exist;
+  }
+
+  if (is_join != P_is_join || init_ui == 0) {
+    if (is_join) {
+      tft.fillRect(210, 45, 110, 15, TFT_BLACK);
+      tft.setTextColor(TFT_GREEN);
+      tft.drawString("connected", 210, 45, GFXFF);
+    }
+    else {
+      tft.fillRect(210, 45, 110, 15, TFT_BLACK);
+      tft.setTextColor(TFT_RED);
+      tft.drawString("disconnect", 210, 45, GFXFF);
+    }
+    P_is_join = is_join;
+  }
+
+
+  tft.setFreeFont(FM9);
+  tft.setTextColor(TFT_WHITE);
+  xOffset = HIST_X_OFFSET + 10 + 50;
+  yOffset = HIST_Y_OFFSET + 50;
+
+
+  //  if (temp != P_temp || init_ui == 0) {
+  //    //    tft.fillRect(90, 88, 25, 25, TFT_BLACK);
+  //    //    tft.drawNumber(25, 90, 88, GFXFF);    // temp  MARK
+  //    P_temp = temp;
+  //  }
+  //
+  //  if (humi != P_humi || init_ui == 0) {
+  //    tft.fillRect(210, 88, 80, 25, TFT_BLACK);
+  //    tft.drawString(result1, 200, 88, GFXFF);   // humi MARK
+  //    P_humi = humi;
+  //  }
+
+  if (N_date != P_date) {
+    tft.fillRect(xOffset, yOffset, 150, 18, TFT_BLACK);
+    tft.drawString(N_date, xOffset + 15, yOffset, GFXFF);
+    P_date = N_date;
+  }
+  yOffset += 18;
+  if (N_time != P_time) {
+    tft.fillRect(xOffset, yOffset, 150, 18, TFT_BLACK);
+    tft.drawString(N_time, xOffset + 15, yOffset, GFXFF);
+    P_time = N_time;
+  }
+  yOffset += 18;
+  if (N_lat != P_lat) {
+    tft.fillRect(xOffset, yOffset, 150, 18, TFT_BLACK);
+    tft.drawString(N_lat, xOffset + 15, yOffset, GFXFF);
+    P_lat = N_lat;
+  }
+  yOffset += 18;
+  if (N_lng != P_lng) {
+    tft.fillRect(xOffset, yOffset, 150, 18, TFT_BLACK);
+    tft.drawString(N_lng, xOffset + 15, yOffset, GFXFF);
+    P_lng = N_lng;
+  }
+  yOffset += 18;
+  if (N_meters != P_meters) {
+    tft.fillRect(xOffset, yOffset, 150, 18, TFT_BLACK);
+    tft.drawString(N_meters, xOffset + 15, yOffset, GFXFF);
+    P_meters = N_meters;
+  }
+  xOffset += 70;
+  yOffset += 18;
+  if (N_satellites != P_satellites) {
+    tft.fillRect(xOffset, yOffset, 100, 18, TFT_BLACK);
+    tft.drawString(N_satellites, xOffset + 15, yOffset, GFXFF);
+    P_satellites = N_satellites;
+  }
+  init_ui = 1;
+}
+
+void setup(void) {
+
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("starting 1...");
+  delay(1000);
+  tft.begin();
+  tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+  pinMode(WIO_5S_LEFT, INPUT_PULLUP);
+  pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
+  lis.begin(Wire1);
+  Serial.println("starting 2...");
+  lis.setOutputDataRate(LIS3DHTR_DATARATE_100HZ); // Setting output data rage to 25Hz, can be set up tp 5kHz
+  lis.setFullScaleRange(LIS3DHTR_RANGE_16G); // Setting scale range to 2g, select from 2,4,8,16g
+
+  GpsSerialInit();
+  softSerial2.begin(9600);
+  softSerial2.listen();
+  while (!softSerial2);
+  Serial.print("E5 LORAWAN TEST\r\n");
+  Serial.println("starting 3...");
+
+  Serial.println("Init finish");
+
+}
+
+
+void loop(void) {
+  static long cTime = 5 * 60 * 1000;
+  static long tTime = 5 * 60 * 1000;
+  long sTime = millis();
+  float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = {0};
+
+  if (switch_UI == 0) {
+    refreshGpsInfo();
+  }
+
+  if (switch_UI == -1) {
+    display_EVI();
+  }
+
+
+  if (digitalRead(WIO_5S_LEFT) == LOW) {
+    tft.fillScreen(TFT_BLACK);
+    init_ui = 0;
+    switch_UI = -1;
+
+  }
+
+  if (digitalRead(WIO_5S_RIGHT) == LOW) {
+    tft.fillScreen(TFT_BLACK);
+    init_ui = 0;
+    switch_UI = 0;
+  }
+  //  GpsSerialInit();
+  GetGpsInfoPolling();
+  UpdateGpsInfo();
+//  Serial.println(N_lat);
+//  Serial.println(N_date);
+
+
+  ////////////////////////////////////////////////////////////////////
+
+  if (tTime > 10 * 1000) {
+    tTime = 0;
+    delay(500);
+    Serial.println("starting sampling...");
+    delay(500);
+    GpsstopListening();
+    for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += 3) {
+      uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
+      lis.getAcceleration(&buffer[ix], &buffer[ix + 1], &buffer[ix + 2]);
+      buffer[ix + 0] *= CONVERT_G_TO_MS2;
+      buffer[ix + 1] *= CONVERT_G_TO_MS2;
+      buffer[ix + 2] *= CONVERT_G_TO_MS2;
+      delayMicroseconds(next_tick - micros());
+    }
+
+    signal_t signal;
+    int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+
+    ei_impulse_result_t result = { 0 };
+
+    err = run_classifier(&signal, &result, debug_nn);
+
+    Serial.println("##################################################");
+    Serial.print("Stop: ");
+    Serial.println(result.classification[0].value);
+    Stop = result.classification[0].value;
+    Serial.print("Turn: ");
+    Serial.println(result.classification[1].value);
+    Turn = result.classification[1].value;
+    Serial.print("Wave: ");
+    Serial.println(result.classification[2].value);
+    Wave = result.classification[2].value;
+    Serial.println("##################################################");
+    if (result.classification[0].value > 0.7) {
+      tft.fillRect(200, 88, 60, 25, TFT_BLACK);
+      tft.drawString("Stop", 200, 88, GFXFF);
+      Stop = result.classification[0].value;
+    }
+    if (result.classification[1].value > 0.7) {
+      tft.fillRect(200, 88, 60, 25, TFT_BLACK);
+      tft.drawString("Turn", 200, 88, GFXFF);
+      Turn = result.classification[1].value;
+    }
+    if (result.classification[2].value > 0.7) {
+      tft.fillRect(200, 88, 60, 25, TFT_BLACK);
+      tft.drawString("Wave", 200, 88, GFXFF);
+      Wave = result.classification[2].value;
+    }
+    GpsSerialInit();
+
+  }
+
+
+  ////////////////////////////////////////////////////////////////////
+
+
+  switch (check_message_response()) {
+    case MODULE_IDLE:
+      if (module_AT_Cmd <= AT_PORT) {
+        at_send_check_response_flag(E5_Module_Cmd[module_AT_Cmd].timeout_ms, E5_Module_Cmd[module_AT_Cmd].p_cmd);
+        Serial.print("module_AT_Cmd = ");
+        Serial.println(module_AT_Cmd);
+      }
+      else if (is_join == true) {
+        if (cTime > 10 * 1000)
+        {
+          cTime = 0;
+          char cmd[128];
+
+          module_AT_Cmd = AT_CMSGHEX;
+          sprintf(cmd, "AT+CMSGHEX=\"%04X%04X%04X\"\r\n", (int)(Stop * 10000), (int)(Turn * 10000), (int)(Wave * 10000));
+          at_send_check_response_flag(E5_Module_Cmd[module_AT_Cmd].timeout_ms, cmd);
+          Serial.println("Send Data");
+        }
+      }
+      else if (is_join == false) {
+        if (cTime > 5 * 1000) {
+          Serial.println("Send Join");
+          cTime = 0;
+          module_AT_Cmd = AT_JOIN;
+          at_send_check_response_flag(E5_Module_Cmd[module_AT_Cmd].timeout_ms, E5_Module_Cmd[module_AT_Cmd].p_cmd);
+        }
+      }
+
+      break;
+    case MODULE_RECEIVING:
+      break;
+    case MODULE_TIMEOUT:
+      is_exist = false;
+      is_join  = false;
+      module_AT_Cmd = AT_OK;
+      Serial.println("MODULE_TIMEOUT");
+      break;
+    case MODULE_ACK_SUCCESS:
+      is_exist = true;
+
+      switch (module_AT_Cmd) {
+        case AT_JOIN:
+          if (strstr(recv_buf, "Network joined") != NULL) {
+            is_join = true;
+          }
+          else {
+            is_join = false;
+          }
+          break;
+
+        case AT_ID:
+          int j = 0;
+          char *p_start = NULL;
+          p_start = strstr(recv_buf, "DevEui");
+          sscanf(p_start, "DevEui, %23s,", &str);//&E5_Module_Data.DevEui);
+          j = 0;
+          for (int i = 0; i < 16; i++, j++) {
+            if ((i != 0) && (i % 2 == 0)) {
+              j += 1;
+            }
+            str_devEui[i] = str[j];
+          }
+          Serial.println(str_devEui);
+          p_start = strstr(recv_buf, "AppEui");
+          sscanf(p_start, "AppEui, %23s,", &str);//&E5_Module_Data.AppEui);
+          j = 0;
+          for (int i = 0; i < 16; i++, j++) {
+            if ((i != 0) && (i % 2 == 0)) {
+              j += 1;
+            }
+            str_appEvi[i] = str[j];
+          }
+          Serial.println(str_appEvi);
+          init_ui = 0;
+          break;
+
+      }
+
+      if (module_AT_Cmd <= AT_PORT) {
+        module_AT_Cmd++;
+      }
+      break;
+  }
+  delay(10);
+  long duration = millis() - sTime;
+  if ( duration < 0 ) duration = 10;
+  cTime += duration;
+  tTime += duration;
+}
--- README.md
+++ README.md
@@ -1,1 +1,146 @@
-# AIoTs_GPS_state_tester
+# LoRa Node with AIoTs_GPS_state_tester
+
+## **Introduction**
+
+The AIoTs GPS & state tester is basic on Wio Terminal Chassis-LoRa-E5 and GNSS to develop. It compares with the traditional IoTs has more concise and intelligent. IoTs basically just receive data then do a command action, whatever data is correct, but AIoTs is able to filer the useless data strictly to get the accurate data by an algorithm which is the neural network.
+In this project, I am using a built-in 3 axis sensor and the neural network to build up an intelligent recognition action system, it can according to your shake Wio Terminal then currently show you its state. Basically, I have trained in three states which are Stop(WT idle state), Turn(rolling-over WT device) and Wave(take WT to wave your hand). If you want to add more states, please go to [Edge Impulse](https://www.edgeimpulse.com/) training the action as you want.
+
+
+
+  <div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/AIoTs_WioTerminal/AIoTs_pic.jpg"/></div>
+
+ ## **Feature**
+
+ - The LoRa device can display the DevEui, APPEui and Appkey on the first page.
+ - Neural network algorithm correct data
+ - High accurately detect Wio Terminal state
+ - Display longitude, latitude and satellites number.
+ - Display the device and TTN connection status.
+  
+
+## **Hardware** 
+
+This demo you will need the device list as below:
+
+- [**WioTerminal**](https://www.seeedstudio.com/Wio-Terminal-p-4509.html)
+
+- [**Wio Terminal Chassis - LoRa-E5 and GNSS**](https://www.seeedstudio.com/Wio-Terminal-Chassis-LoRa-E5-and-GNSS-p-5053.html)
+
+- [**Wio Terminal Chassis - Battery (Optional)**](https://www.seeedstudio.com/Wio-Terminal-Chassis-Battery-650mAh-p-4756.html)
+
+
+
+
+## **Usage**
+
+This project is based on the Arduino platform which means we’ll be using the Arduino IDE and various Arduino libraries. If this is your first time using the Wio terminal, here is a guide to quickly [**Get Started with Wio Terminal**](https://wiki.seeedstudio.com/Wio-Terminal-Getting-Started/).
+
+
+Requite library:
+- [**Seeed_Arduino_SFUD**](https://github.com/Seeed-Studio/Seeed_Arduino_SFUD)
+
+### **Edge Impulse training**
+If you want to add more action to recognized on Wio terminal, please go to the Edge Impulse to training a new library as you want action.
+there is training instruction: 
+
+Step 1. 
+Open the [**Edge Impulse website**](https://studio.edgeimpulse.com/studio/select-project?autoredirect=1), and regiter a account. 
+
+Step 2. 
+Create a new project.
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/AIoTs_WioTerminal/edge_PIC1.png"/></div>
+
+Step 3.
+Download the [**Edge Impulse firmware**](https://github.com/Seeed-Studio/Seeed_Arduino_edgeimpulse/releases/tag/1.4.0) for connect to webside.  
+
+Double click the left button on Wio terminal, you will see a driver popup, then draw the firmware to the driver, the driver will disappear which mean the firmware has been uploaded. More detail on [**the Wio terminal get start**](https://wiki.seeedstudio.com/Wio-Terminal-Getting-Started/).
+
+Step 4.
+When you click "connect using WebUSB", the Wio Terminal will connect to the website, then there you can start to training the action as you want.
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/AIoTs_WioTerminal/Edge_PIC2.png"/></div>
+
+Step 5. 
+When you finish all step, you need to select "Deployment" to general you trained library, then put on the third-party library. 
+
+
+
+Note
+If you need konw more about the Edge Impulse or something still unclear, please go to [**Wio Terminal Edge Impulse Getting Started**](https://wiki.seeedstudio.com/Wio-Terminal-TinyML-EI-1/).
+
+
+
+### **TheThingsNetwork Console Configuration Setup**
+
+In this project, The data will display on [**TheThingsNetwork**](https://www.thethingsnetwork.org) platform, the instuction as below:
+
+Step 1: Load into [**TTN website**](https://www.thethingsnetwork.org) and create your account, then go to gateways start to set up your device.
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_gataway1.png"/></div>
+
+Step 2: Add the gateway device:
+- Owner
+- Gteway ID
+- Gateway EUI
+- Gateway name
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_gateway2.png"/></div>
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_gateway3.png"/></div>
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_gateway4.png"/></div>
+
+Step 3: Add Application:
+- Owner
+- Application ID
+- Application name
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_applications.png"/></div>
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/application2.png"/></div>
+
+Step 4：Add the LoRa node:
+- Brand (Select Sense CAP)
+- Model (Select LoRa-E5)
+- Hardware Ver (Defult)
+- Firmware Ver (Defult)
+- Profile (The Region is according to your location)
+- Frequency plan
+- AppEUI
+- DEVEUI
+- AppKey
+- End Device ID
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_device1.png"/></div>
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_device4.png"/></div>
+
+Step 5: Add the code for decode the data:
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_decode1.png"/></div>
+
+```CPP
+function Decoder(bytes, port) {
+ 
+  var decoded = {};
+  if (port === 8) {
+    decoded.Stop   = bytes[1];
+    decoded.Turn   = bytes[3];
+    decoded.Wave   = bytes[5];
+  }
+ 
+  return decoded;
+}
+
+ 
+```
+
+Step 5: Cheack the result on TheThingsNetwork
+
+Go to the geteway, then click "Live data".
+
+<div align=center><img width = 500 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/TTN_dataTEMP1.png"/></div>
+
+
+Each LoRa device has a unique serial number, after you connect the LoRa device to the Wio terminal then there will display the deveui, appeui and appkey on the first page, you need to fill the LoRa ID and gateway ID in server.
+
+<div align=center><img width = 600 src="https://files.seeedstudio.com/wiki/LoRa_WioTerminal/temp_ID.png"/></div>
